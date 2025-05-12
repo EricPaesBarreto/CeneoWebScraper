59a9d18c59b601532d0c586c77d6b195cd55535b
ericpaesbarreto <ericpaesbarreto@outlook.com>
2025-05-12 16:58:32 +0200

    2025.05.12 - ericpaesbarreto - created basic html templates and updated routes to match.

--- app/routes.py
+++ app/routes.py
@@ -6,10 +6,25 @@ from app import app
     # post : data is visible
 
 @app.route("/")
-@app.route("/<name>") # get
-def index(name="N/A"):
-    return render_template("index.html", name=name)
+def index():
+    return render_template("index.html")
+
+@app.route("/extract/")
+def extract():
+    return render_template("extract.html")
 
 @app.route("/product/<product_id>")
-def product(product_id="N/A"):
-    return render_template("index.html", product_id=product_id)
+def product(product_id):
+    return render_template("product.html", product_id=product_id)
+
+@app.route("/charts/<product_id>")
+def charts():
+    return render_template("charts.html", product_id=product_id)
+
+@app.route("/products")
+def index():
+    return render_template("products.html")
+    
+@app.route("/about")
+def about():
+    return render_template("about.html")
